642ef33a54516d9946d6f362eb0b5945fece5b4e
akaps999 <112928343+akaps999@users.noreply.github.com>
2022-10-19 10:29:50 +0900

    Add files via upload

---
+++ chapter5/c._if_pl5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1 = 20, num2 = 10, num3 = 50;
+    int temp;
+    // 정렬: num1 > num2 > num3
+    // 조건1: num1 < num2 → 값 교환 : num1 > num2
+    // 조건2: num1 < num3 → 값 교환 : num1 > num2; num3
+    // 조건3: num2 < num3 → 값 교환 : num2 > num3
+    // → num1 > num2 > num3
+
+
+    // 변수 3개에 각각 다른 값을 담음
+    //  - 문제: num1과 num2에 값을 바꿔(체인지!)
+
+    // 코드 ---
+        // 체인지 코드 작성!!
+        //num1 = 10 xxx
+        //num2 = 20 xxx
+    // 코드 ---
+
+    if(num1 < num2) { 
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    if(num1 < num3) { 
+        temp = num1;
+        num1 = num3;
+        num3 = temp;
+    }
+    if(num2 < num3) { 
+        temp = num2;
+        num2 = num3;
+        num3 = temp;
+    }
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    printf("%d > %d > %d", num1, num2, num3);
+
+    
+    //printf("num1: %d, num1"); // 10
+    //printf("num2: %d, num2"); // 20
+
+}
