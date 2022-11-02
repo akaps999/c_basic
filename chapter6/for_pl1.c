9c93adae8da7b303c185a8bbd68083fd4ed2f951
akaps999 <112928343+akaps999@users.noreply.github.com>
2022-11-02 10:37:19 +0900

    Add files via upload

---
+++ chapter6/for_pl1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int sum = 0;  // 총합을 누적 시킴
+
+    //작성해보새요. (for문 활용)
+    //1~10까지 값이 1씩 증가! i대체!
+    //for문 활용해서 1~10까지 값을 덧셈하는 코드작성
+    for(int i=1; i<=10; i++) {
+        sum = sum + i;
+        // sum += i;
+    }
+    printf("누적 총합: %d\n", sum);
+}
+
+// 위에서 for(int i=1; i<=10; i++) 일때, i=4 sum 값은 9가 된다.
--- chapter6/gugudan.c
+++ chapter6/gugudan.c
@@ -9,7 +9,7 @@
  *    2 x 9 = 18
 */
 int main(void) {
-    for(int i = 1; i<= 9; i++) {
+    for(int i = 1; i <= 9; i++) {
         printf("2 x %d = %d\n", i, 2*i);
     }
 }
